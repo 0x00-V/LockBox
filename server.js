9d0d73b1b4888f0fd4c6e2d405a3d31177cd5e28
Daniel Thomas <daniel@null.com>
2025-10-14 17:10:06 +0100

    Fixed PFPs and fixed Nav

--- server.js
+++ server.js
@@ -70,14 +70,9 @@ app.get("/", async (req, res) => {
   if(!sessionId) return res.sendFile(path.join(process.cwd(), "public", 'index.html'));
   try {
     const result = await client.query('SELECT users.username FROM sessions JOIN users ON sessions.user_id = users.id WHERE sessions.session_id = $1 AND sessions.expires_at > NOW()', [sessionId]);
-    if(result.rows.length > 0) {
-      return res.redirect("/dashboard");
-    } else{
-      return res.sendFile(path.join(process.cwd(), "public", "index.html"));
-    }
-  } catch(err){
-      return res.status(404);
-  }
+    if(result.rows.length > 0) { return res.redirect("/dashboard");
+    } else{ return res.sendFile(path.join(process.cwd(), "public", "index.html")); }
+  } catch(err){ return res.status(404); }
 });
 app.use(express.static("public"));
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
@@ -95,15 +90,10 @@ app.post("/login", async (req, res) => {
   const {username, password} = req.body;
   try{
     const q1_resp = await client.query("SELECT id, username, password FROM users WHERE username = $1", [username]);
-
-    if(q1_resp.rows.length === 0){
-      return res.send(`<script> alert("Incorrect Credentials."); window.location.href = "/login" </script>`);
-    }
+    if(q1_resp.rows.length === 0){ return res.send(`<script> alert("Incorrect Credentials."); window.location.href = "/login" </script>`); }
     const user = q1_resp.rows[0];
     const valid = await bcrypt.compare(password, user.password);
-    if(!valid){
-     return res.send(`<script> alert("Incorrect Credentials."); window.location.href = "/login" </script>`);
-    }
+    if(!valid){ return res.send(`<script> alert("Incorrect Credentials."); window.location.href = "/login" </script>`); }
     const sessionId = generateSessionId();
     await client.query("INSERT INTO sessions (session_id, user_id, expires_at) VALUES ($1, $2, NOW() + interval '1 hour')", [sessionId, user.id]);
       res.cookie("sessionId", sessionId, {httpOnly: true, secure: false, sameSite: "lax", maxAge: 3600 * 1000});
@@ -158,6 +148,7 @@ app.get("/api/modules/coding", async (req, res) => {
   }
 });
 
+
 app.get("/api/modules/cybersecurity", async (req, res) => {
   try{
     const sessionId = req.cookies.sessionId;
@@ -198,20 +189,19 @@ app.post("/api/modules/:id/pin", async (req, res) => {
   const user_id = usr_result.rows[0].id;
   const module_id = req.params.id;
   try{
-  const check = await client.query("SELECT pinned FROM user_module_data WHERE user_id = $1 AND module_id = $2", [user_id, module_id]);
-  if(check.rows.length > 0){
+    const check = await client.query("SELECT pinned FROM user_module_data WHERE user_id = $1 AND module_id = $2", [user_id, module_id]);
+    if(check.rows.length > 0){
       const newly_pinned = !check.rows[0].pinned;
       await client.query("UPDATE user_module_data SET pinned = $1 WHERE user_id = $2 AND module_id = $3", [newly_pinned, user_id, module_id]);
       return res.json({pinned: newly_pinned});
-  } else {
-    await client.query("INSERT INTO user_module_data (user_id, module_id, pinned) VALUES ($1, $2, true)", [user_id, module_id]);
-    return res.json({pinned: true});
-  }
+    } else {
+      await client.query("INSERT INTO user_module_data (user_id, module_id, pinned) VALUES ($1, $2, true)", [user_id, module_id]);
+      return res.json({pinned: true});
+    }
   } catch (err) {
     console.error(err);
     res.status(500).json({erorr: "Failed to toggle"});
   }
-
 });
 
 
@@ -222,13 +212,9 @@ app.post("/api/account/upload-avatar", upload.single("avatar"), async (req, res)
   if(usr_result.rows.length === 0) return res.status(401).json({erorr: "Unauthorised"});
   const user_id = usr_result.rows[0].id;
   const old_avatar = usr_result.rows[0].avatar;
-  if(old_avatar && !old_avatar.includes("template-icon.png"))
-  {
-    try{
-      await fsp.unlink(path.join(process.cwd(), old_avatar));
-    } catch (err) {
-      console.error("Failed to delete", err);
-    }
+  if(old_avatar && !old_avatar.includes("template-icon.png")) {
+    try{ await fsp.unlink(path.join(process.cwd(), old_avatar)); }
+    catch (err) { console.error("Failed to delete", err); }
   }  
   const avatarPath = `/uploads/avatars/${req.file.filename}`;
   await client.query("UPDATE users SET avatar = $1 WHERE id = $2", [avatarPath, user_id]);
@@ -242,9 +228,7 @@ app.get("/dashboard", async (req, res) => {
   const result = await client.query("SELECT users.username, users.avatar FROM sessions JOIN users ON sessions.user_id = users.id WHERE sessions.session_id = $1 AND sessions.expires_at > NOW()", [sessionId]);
   if(result.rows.length === 0) return res.redirect("/login");
   const username = result.rows[0].username;
-  const avatar = await getAvatar(result.rows[0].avatar);
-
- 
+  const avatar = await getAvatar(result.rows[0].avatar); 
   let dashboard_html = await fsp.readFile(
   path.join(process.cwd(), "public", "dashboard.html"), { encoding: "utf8" });
   dashboard_html = dashboard_html.replace("{{username}}", username).replace(/{{avatar}}/g, avatar);
@@ -258,7 +242,7 @@ app.get("/account_settings", async (req, res) => {
   const result = await client.query("SELECT users.username, users.avatar FROM sessions JOIN users ON sessions.user_id = users.id WHERE sessions.session_id = $1 AND sessions.expires_at > NOW()", [sessionId]);
   if(result.rows.length === 0) return res.redirect("/login");
   const username = result.rows[0].username;
-  const avatar = result.rows[0].avatar || "/uploads/avatars/template-icon.png"
+  const avatar = await getAvatar(result.rows[0].avatar);
   let account_settings_html = await fsp.readFile(
   path.join(process.cwd(), "public", "account_settings.html"), { encoding: "utf8" });
   account_settings_html = account_settings_html.replace("{{username}}", username).replace(/{{avatar}}/g, avatar);
@@ -273,7 +257,7 @@ app.get("/learn", async (req, res) => {
   const result = await client.query("SELECT users.username, users.avatar FROM sessions JOIN users ON sessions.user_id = users.id WHERE sessions.session_id = $1 AND sessions.expires_at > NOW()", [sessionId]);
   if(result.rows.length === 0) return res.redirect("/login");
   const username = result.rows[0].username;
-  const avatar = result.rows[0].avatar || "/uploads/avatars/template-icon.png"
+  const avatar = await getAvatar(result.rows[0].avatar);
   let learn_html = await fsp.readFile(
   path.join(process.cwd(), "public", "learn.html"), { encoding: "utf8" });
   learn_html = learn_html.replace("{{username}}", username).replace(/{{avatar}}/g, avatar);
@@ -299,12 +283,7 @@ app.get("/learning", async (req, res) => {
     module_description = "Unable to load module data.";
     module_content = "Unable to load module data.";
   }
-  let avatar = result.rows[0].avatar;
-  try{
-    await fsp.access(path.join(process.cwd(), avatar));
-  } catch {
-    avatar = "/uploads/avatars/template-icon.png";
-  }
+  const avatar = await getAvatar(result.rows[0].avatar);
   let learning_html = await fsp.readFile(path.join(process.cwd(), "public", "learning.html"), { encoding: "utf8" });
   learning_html = learning_html.replace("{{module_title}}", module_title).replace("{{module_description}}", module_description).replace("{{module_content}}", module_content).replace(/{{avatar}}/g, avatar);
   res.send(learning_html);
