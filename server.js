7f504b1f41aeb9e8e08458a4b01fbb5cfb21500d
Daniel Thomas <business.danielthomas@outlook.com>
2025-10-21 17:53:46 +0100

    Removed Comma

--- server.js
+++ server.js
@@ -431,7 +431,7 @@ CREATE TABLE users (
   username VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
   avatar VARCHAR(255) DEFAULT '/uploads/avatars/template-icon.png',
-  role VARCHAR(255), DEFAULT 'user'
+  role VARCHAR(255) DEFAULT 'user'
 );
 
 CREATE TABLE sessions (
